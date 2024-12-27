b63cd2fd5f87e917be7dd73a8d1209774a03c899
kenanmusali <textme0306@gmail.com>
2024-12-27 20:34:00 +0400

    v0.2.9.5

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  HostListener } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -17,5 +17,22 @@ export class MainComponent implements OnInit {
       top: 0,
       behavior: 'smooth' 
     });
+  }
+
+  isToggled = false;  // Flag to track whether the sidebar is toggled
+
+  // Method to toggle the state of 'isToggled' on click inside the sidebar
+  toggleImage(event: MouseEvent): void {
+    event.stopPropagation();  // Prevent click from propagating to document body
+    this.isToggled = !this.isToggled;  // Toggle the value of 'isToggled' between true/false
+  }
+
+  // HostListener to listen for clicks outside the sidebar
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: MouseEvent): void {
+    const sidebarElement = document.querySelector('.SideBarAccount');
+    if (sidebarElement && !sidebarElement.contains(event.target as Node)) {
+      this.isToggled = false;  // Close the sidebar if click happens outside
+    }
   }
 }
